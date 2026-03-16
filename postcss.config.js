b5ee8752fde2f6ae3865d13e7943d5bf6957f60e
Issa Tchamo <sanha@mac.home>
2026-03-16 18:42:09 +0000

    item add to LocalStorage

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 import autoprefixer from "autoprefixer";
-import cssnanoPlugin from "cssnano";
+
 export default {
   plugins: {
     "@tailwindcss/postcss": {},
     autoprefixer: {},
-    cssnanoPlugin:{},
+
   },
 };
